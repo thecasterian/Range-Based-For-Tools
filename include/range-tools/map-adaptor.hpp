d13be012c2eaeff955f5bc8bc4bc501812d8189a
Jeongu Kim <jeongukim@kaist.ac.kr>
2023-05-28 03:01:34 +0900

    Refactor map_keys

--- include/range-tools/map-adaptor.hpp
+++ include/range-tools/map-adaptor.hpp
@@ -11,6 +11,69 @@ namespace range_tools {
 namespace internal {
 
 template <typename MapIter>
+class MapKeysIterator {
+public:
+    using value_type = typename std::decay_t<DereferIterator<MapIter>>::first_type;
+    using reference = const value_type &;
+
+    MapKeysIterator(const MapIter &iter) : _iter(iter) {}
+
+    MapKeysIterator &operator++() {
+        ++_iter;
+        return *this;
+    }
+
+    MapKeysIterator operator++(int) {
+        auto tmp = *this;
+        ++_iter;
+        return tmp;
+    }
+
+    bool operator==(const MapKeysIterator<MapIter> &other) const {
+        return _iter == other._iter;
+    }
+
+    bool operator!=(const MapKeysIterator<MapIter> &other) const {
+        return _iter != other._iter;
+    }
+
+    reference operator*() const {
+        return _iter->first;
+    }
+
+private:
+    MapIter _iter;
+};
+
+template <typename Map>
+class MapKeys {
+public:
+    using iterator = MapKeysIterator<Iterator<Map>>;
+    using reverse_iterator = MapKeysIterator<ReverseIterator<Map>>;
+
+    MapKeys(Map &&map) : _map(std::forward<Map>(map)) {}
+
+    iterator begin(void) {
+        return iterator(_map.begin());
+    }
+
+    iterator end(void) {
+        return iterator(_map.end());
+    }
+
+    reverse_iterator rbegin(void) {
+        return reverse_iterator(_map.rbegin());
+    }
+
+    reverse_iterator rend(void) {
+        return reverse_iterator(_map.rend());
+    }
+
+private:
+    Map _map;
+};
+
+template <typename MapIter>
 class MapValuesIterator {
 public:
     using value_type = typename std::decay_t<DereferIterator<MapIter>>::second_type;
@@ -29,11 +92,11 @@ public:
         return tmp;
     }
 
-    bool operator==(const MapValuesIterator &other) const {
+    bool operator==(const MapValuesIterator<MapIter> &other) const {
         return _iter == other._iter;
     }
 
-    bool operator!=(const MapValuesIterator &other) const {
+    bool operator!=(const MapValuesIterator<MapIter> &other) const {
         return _iter != other._iter;
     }
 
@@ -53,19 +116,19 @@ public:
 
     MapValues(Map &&map) : _map(std::forward<Map>(map)) {}
 
-    iterator begin() {
+    iterator begin(void) {
         return iterator(_map.begin());
     }
 
-    iterator end() {
+    iterator end(void) {
         return iterator(_map.end());
     }
 
-    reverse_iterator rbegin() {
+    reverse_iterator rbegin(void) {
         return reverse_iterator(_map.rbegin());
     }
 
-    reverse_iterator rend() {
+    reverse_iterator rend(void) {
         return reverse_iterator(_map.rend());
     }
 
@@ -74,6 +137,11 @@ private:
 };
 
 }  // namespace internal
+
+template <typename Map>
+auto map_keys(Map &&map) {
+    return internal::MapKeys<Map>(std::forward<Map>(map));
+}
 
 template <typename Map>
 auto map_values(Map &&map) {
